fbddd30883ce89b5390aac4d2de73cb8251b626f
tahapek5454 <tahapek5454@gmail.com>
2023-12-02 18:56:11 +0300

    feat: add event queuename

---
+++ Shared/RabbitMQ/RabbitMQSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.RabbitMQ
+{
+    public static class RabbitMQSettings
+    {
+        public const string Stock_OrderCreatedEventQueue = "stock_order_created_event_queue";
+    }
+}
