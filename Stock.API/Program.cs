aa5c96c670a36b0d32cffa239d3820ff12e5b30f
tahapek5454 <tahapek5454@gmail.com>
2023-12-03 14:30:27 +0300

    feat: add info middleware

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Shared.RabbitMQ;
 using Stock.API.Consumers;
 using Stock.API.Models.DbContexts;
+using System.Globalization;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,5 +40,41 @@ builder.Services.AddDbContext<StockAPIDbContext>(option =>
 
 var app = builder.Build();
 
+
+// show product Id in stock
+app.Use(async (context, next) =>
+{
+    
+    using IServiceScope scope = app.Services.CreateScope();
+
+    StockAPIDbContext? dbContext = scope.ServiceProvider.GetService<StockAPIDbContext>();
+
+    if(dbContext != null)
+    {
+        var stocks = dbContext.Stocks.ToList();
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.AppendLine($"Stokta Bulunan Ürün Numaralarý Listesi");
+
+        int counter = 1;
+        foreach (var stock in stocks)
+        {
+            stringBuilder.AppendLine($"{counter}-) ${stock.ProductId}");
+
+            counter++;
+        }
+
+
+        await context.Response.WriteAsync( stringBuilder.ToString() );  
+    }
+    else
+    {
+        await context.Response.WriteAsync("Seed datalar þu an için gösterilemiyor");
+    }
+    
+    await next(context);
+});
 
 app.Run();
